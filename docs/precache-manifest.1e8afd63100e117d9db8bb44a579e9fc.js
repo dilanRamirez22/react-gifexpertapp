ec8f4cb2c47a52c37bbce66ff7b6e7337526c162
dilan ramirez <dilanrmrz25@gmail.com>
2020-11-05 11:36:36 -0500

    docs agregado

---
+++ docs/precache-manifest.1e8afd63100e117d9db8bb44a579e9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ace55d594d52a3f501655c948be1840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1e25a3f601fbdf4f920",
+    "url": "/static/css/main.e95fb739.chunk.css"
+  },
+  {
+    "revision": "d28c81c87f670f22e8cb",
+    "url": "/static/js/2.eb59c0d7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.eb59c0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e25a3f601fbdf4f920",
+    "url": "/static/js/main.625b1f2d.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);

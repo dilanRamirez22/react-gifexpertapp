c48e77b6242dc840b995202fcb5141abf89b67ba
dilan ramirez <dilanrmrz25@gmail.com>
2020-11-05 13:43:57 -0500

    problema https

--- src/assets/js/gifFetch.js
+++ src/assets/js/gifFetch.js
@@ -1,7 +1,7 @@
 
 export const gifFetch=async(categoria)=>{
 
-    const url=`http://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(categoria)}&api_key=44VyrZbkuSjPiI5HY9OAqSePsiOUqrPB`
+    const url=`https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(categoria)}&api_key=44VyrZbkuSjPiI5HY9OAqSePsiOUqrPB`
     const res=await fetch(url);
     const data=await res.json();
 
